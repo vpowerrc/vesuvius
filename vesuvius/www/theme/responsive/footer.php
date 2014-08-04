ca54c9aad116f69383bfd64627d37d1ebfe83d8a
Viduranga Wijesooriya <vpowerrc@gmail.com>
2014-08-04 11:09:13 +0530

    added responsive theme
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

---
+++ vesuvius/www/theme/responsive/footer.php
@@ -0,0 +1,38 @@
+<?
+/**
+ * @name         Lost Person Finder Theme
+ * @version      3.0
+ * @package      lpf
+ * @author       Greg Miernicki <g@miernicki.com> <gregory.miernicki@nih.gov>
+ * @about        Developed in whole or part by the U.S. National Library of Medicine
+ * @link         https://pl.nlm.nih.gov/about
+ * @link         http://sahanafoundation.org
+ * @license	 http://www.gnu.org/licenses/lgpl-2.1.html GNU Lesser General Public License (LGPL)
+ * @lastModified 2011.0816
+ */
+
+
+$legaleseLink = "OMB NO: 0925-0612 EXPIRATION DATE: 6/30/2013";
+
+$legalese = "Notice: Submission of information is voluntary. All submitted information will be made publicly available. OMB NO: 0925-0612 EXPIRATION DATE: 6/30/2013 Public reporting burden for this collection of information is estimated to average 0.08 hours per response. This estimate includes the time for reviewing instructions, gathering, and entering data. An agency may not conduct or sponsor, and a person is not required to respond to, a collection of information unless it displays a currently valid OMB control number. Send comments regarding this burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden, to: NIH, Project Clearance Branch, 6705 Rockledge Drive, MSC 7974, Bethesda, MD 20892-7974, ATTN: PRA (0925-0612). Do not return the completed form to this address.";
+
+?>
+
+<div id="footer"><center><table id="footerTable">
+		<tr>
+	
+	
+		<td>
+		<div id="sahana_logo">
+			<a href="http://sahanafoundation.org"><img src="theme/responsive
+/img/sahanalogo.png" style='width:100%;' border="0" alt="Sahana Software Foundation Logo"></a><br>
+		</div>	
+		</td>
+		</tr>
+	</table>
+	
+	</center>
+</div>
+<?php
+
+
